5f2bddffa9f1b01a243c80f2e5f4cbf635f56238
aldhitanca888 <aldhitanca888@gmail.com>
2026-04-15 11:48:36 +0700

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 // src/config.js
-// const API_BASE_URL = "http://54.254.107.154/api";
-const API_BASE_URL = "http://localhost:5000";
+// const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://so-op.vercel.app";
 
 export default API_BASE_URL;
